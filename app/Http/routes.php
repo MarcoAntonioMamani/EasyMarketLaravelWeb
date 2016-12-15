ab3235e261be48627e995a89aaa984494e6afa79
MarcoAntonio MamaniChura <marcoantoniomamanichura@gmail.com>
2016-12-14 23:48:18 -0400

    Comit Configuracion

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -13,10 +13,11 @@
 
 Route::get('/', function () {
     return view('welcome');
+    
 });
 
 Route::auth();
-
 Route::get('/home', 'HomeController@index');
 Route::get('/redirect','SocialAuthController@redirect');
 Route::get('/callback','SocialAuthController@callback');
+
